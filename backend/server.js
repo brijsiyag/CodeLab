e3fda835da19b0b3cf70a78a146d9d4c224b1a0e
brijsiyag <brijsiyag@gmail.com>
2021-09-17 01:21:13 +0530

    Before Authentication

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,41 @@ const PORT = process.env.PORT | 5000;
 const request = require('request');
 const mysql = require("mysql");
 const cors = require("cors");
-const { urlencoded } = require("express");
 
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "DbmsProject"
+});
+
 
 app.get("/", (req, res) => {
     res.send("Hiii");
 });
-app.post("/", (req, res) => {
+
+app.get("/questions", (req, res) => {
+    connection.query("SELECT * FROM question", (err, result) => {
+        res.json(result);
+    });
+});
+app.post("/addQuestion", (req, res) => {
+    connection.query(`INSERT INTO question VALUES('${req.body.question_id}', '${req.body.question}', '${req.body.output}', '${req.body.tags}', '${req.body.difficulty_level}', '${req.body.input}')`, (err, result) => {
+        if (err) {
+            res.send({ ...err, error: true });
+        }
+        else {
+            res.send({ ...result, error: false });
+        }
+    })
+})
+
+app.post("/submit", (req, res) => {
+    res.redirect("/");
     const program = {
         script: req.body.code,
         language: req.body.lang,
