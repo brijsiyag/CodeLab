a01489da241b9c7a5660eda8a5c928ae78caaa40
devendr09 <vr.devendrakumar@gmail.com>
2021-10-12 16:00:28 +0530

    Carousel updated

--- frontend/src/HomePage/ImageSlider.js
+++ frontend/src/HomePage/ImageSlider.js
@@ -1,60 +1,49 @@
 import React from "react";
+
 import useState from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import "./ImageSlider.css";
+
 function ControlledCarousel() {
+
+  let img_arr = ["./First.png", "./Second2.png"]
+  let quote_arr = ["“First, solve the problem. Then, write the code.” – John Johnson", "“ Code is like humor. When you have to explain it, it’s bad.” – Cory House"];
+  
   return (
-    <div
-      style={{
-        width: "60vw",
-        margin: "auto",
-        border: "black 2px solid",
-        borderRadius: "5px",
-      }}
-    >
-      <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="d-block carousel-img"
-            src="./First.png"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>
-              “First, solve the problem. Then, write the code.” – John Johnson.
-            </h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block carousel-img"
-            src="./Second.png"
-            alt="Second slide"
-          />
+    <div>
 
-          <Carousel.Caption>
-            <h3>
-              “ Code is like humor. When you have to explain it, it’s bad.” –
-              Cory House
-            </h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block carousel-img"
-            src="./First.png"
-            alt="Third slide"
-          />
+      <div
+        style={{
+          width: "60vw",
+          margin: "auto",
+          border: "black 2px solid",
+          borderRadius: "5px",
+          background: "black"
+        }}
+      >
+        <Carousel>
 
-          <Carousel.Caption>
-            <h3>
-              “ Code is like humor. When you have to explain it, it’s bad.” –
-              Cory House
-            </h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block carousel-img"
+              src={img_arr[0]}
+              alt="First slide"
+            />
+            <div className="carousel_caption"><h3>{quote_arr[0]}</h3></div>
+          </Carousel.Item>
+
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block carousel-img"
+              src={img_arr[1]}
+              alt="Second slide"
+              />
+              <div className="carousel_caption"><h3>{quote_arr[1]}</h3></div>
+          </Carousel.Item>
+
+        </Carousel>
+      </div>
     </div>
   );
 }
